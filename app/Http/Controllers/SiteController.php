f4487e2bea4c81110da842556cea19b8712e5fac
Lutar <oibolutar@gmail.com>
2017-09-16 10:45:00 +0300

    maked home route, created base and index controllers, created layout and index template, placed public elements

---
+++ app/Http/Controllers/SiteController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Corp\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use Corp\Http\Requests;
+
+class SiteController extends Controller
+{
+    protected $p_rep;
+    protected $s_rep;
+    protected $a_rep;
+    protected $m_rep;
+
+    protected $template;
+
+    protected $vars = array();
+
+    protected $contentRightBar = false;
+    protected $contentLeftBar = false;
+
+    protected $bar = false;
+
+
+    public function __construct()
+    {
+
+    }
+
+
+    protected function renderOutput()
+    {
+
+        return view($this->template)->with($this->vars);
+    }
+}
