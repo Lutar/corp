dc82d44b7590a7f59d0f1597e4a53f5422f7bffb
Lutar <oibolutar@gmail.com>
2017-09-16 11:31:38 +0300

    highlighting the menu in a separate layout

--- app/Http/Controllers/SiteController.php
+++ app/Http/Controllers/SiteController.php
@@ -32,6 +32,9 @@ class SiteController extends Controller
     protected function renderOutput()
     {
 
+        $navigation = view(env('THEME').'.navigation');
+        $this->vars = array_add($this->vars, 'navigation', $navigation);
+
         return view($this->template)->with($this->vars);
     }
 }
