043ce4569bb9745e743ef9d3391d54dce6635846
Lutar <oibolutar@gmail.com>
2017-09-28 18:47:03 +0300

    add menu button

--- resources/views/pink/admin/menus_content.blade.php
+++ resources/views/pink/admin/menus_content.blade.php
@@ -1,5 +1,6 @@
 <div id="content-page" class="content group">
     <div class="hentry group">
+
         <h3 class="title_page">Пользователи</h3>
         <div class="short-table white">
             <table style="width: 100%" cellpadding="0" cellspacing="0">
@@ -14,5 +15,8 @@
                 @endif
             </table>
         </div>
+
+        {!! HTML::link(route('admin.menus.create'),'Добавить  пункт',['class' => 'btn btn-the-salmon-dance-3']) !!}
+
     </div>
 </div>
